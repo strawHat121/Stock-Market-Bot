7e74ac16581ab7915abfbee3dd434589b174412a
kinb98 <kinjalkbajpai@gmail.com>
2022-05-30 22:28:15 +0530

    Added the api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { default: axios } = require("axios");
 const { TwitterClient } = require("twitter-api-client");
 
 const twitterClient = new TwitterClient({
@@ -7,3 +8,24 @@ const twitterClient = new TwitterClient({
   accessToken: process.env.TWITTER_ACCESS_TOKEN,
   accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
+
+let data = {};
+
+const options = {
+  method: "GET",
+  url: "https://nse-data1.p.rapidapi.com/all_indices",
+  headers: {
+    "X-RapidAPI-Host": "nse-data1.p.rapidapi.com",
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    data = response.data.body.data[0];
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
